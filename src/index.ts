75e58f22dee7e4edb68621c8022aa4cf55f4a7c5
xiaoli <2585960098@qq.com>
2023-05-28 21:37:11 +0800

    test:first publish test

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import { App } from 'vue'
 import LText from './components/LText'
+import LImage from './components/LImage'
 
-const components = [LText]
+const components = [LText, LImage]
 const install = (app: App) => {
   components.forEach(component => {
     app.component(component.name, component)
   })
 }
 
-export { install, LText }
+export { install, LText, LImage }
 
 export default { install }
